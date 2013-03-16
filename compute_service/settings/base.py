a49c7296bd476176c2b98834ace347f363ff4c63
Harish Narayanan <hnarayanan@gmail.com>
2013-03-16 02:34:12 +0100

    Start working on a snippet model

--- compute_service/settings/base.py
+++ compute_service/settings/base.py
@@ -118,9 +118,12 @@ DJANGO_APPS = (
 
 THIRD_PARTY_APPS = (
     'south',
+    'rest_framework'
 )
 
-LOCAL_APPS = ()
+LOCAL_APPS = (
+    'snippets',
+)
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
--- compute_service/urls.py
+++ compute_service/urls.py
@@ -9,6 +9,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'compute_service.views.home', name='home'),
     # url(r'^compute_service/', include('compute_service.foo.urls')),
 
+    url(r'^', include('snippets.urls')),
+
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
 )
