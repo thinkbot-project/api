5078d7a00daa82c7a89018511311ad8d53a304cd
Harish Narayanan <hnarayanan@gmail.com>
2013-03-23 14:07:01 +0100

    Simplify poisson demo

--- sandbox/fenics-demos/demo_poisson.py
+++ sandbox/fenics-demos/demo_poisson.py
@@ -1,39 +1,3 @@
-"""This demo program solves Poisson's equation
-
-    - div grad u(x, y) = f(x, y)
-
-on the unit square with source f given by
-
-    f(x, y) = 10*exp(-((x - 0.5)^2 + (y - 0.5)^2) / 0.02)
-
-and boundary conditions given by
-
-    u(x, y) = 0        for x = 0 or x = 1
-du/dn(x, y) = sin(5*x) for y = 0 or y = 1
-"""
-
-# Copyright (C) 2007-2011 Anders Logg
-#
-# This file is part of DOLFIN.
-#
-# DOLFIN is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# DOLFIN is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
-#
-# First added:  2007-08-16
-# Last changed: 2012-11-12
-
-# Begin demo
-
 from dolfin import *
 
 # Create mesh and define function space
@@ -59,10 +23,3 @@ L = f*v*dx + g*v*ds
 # Compute solution
 u = Function(V)
 solve(a == L, u, bc)
-
-# Save solution in VTK format
-file = File("poisson.pvd")
-file << u
-
-# Plot solution
-plot(u, interactive=True)
