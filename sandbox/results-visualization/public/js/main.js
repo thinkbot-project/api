b680fec9879f3ea7d32331356276ab8492a3305c
Harish Narayanan <hnarayanan@gmail.com>
2013-03-25 21:43:04 +0100

    Refactor visualization example in sandbox some more

--- sandbox/results-visualization/public/js/main.js
+++ sandbox/results-visualization/public/js/main.js
@@ -16,46 +16,44 @@ window.onload = function() {
 	}
     });
 
-    result = $.getValues("http://localhost:8000/jobs/1/")
+    var renderValues = function(result) {
+	var items = [];
+	items.push('<li><strong>Name:</strong> ' + result.name + '</li>');
+	items.push('<li><strong>Owner:</strong> ' + result.owner + '</li>');
+	items.push('<li><strong>Resource URL:</strong> ' + result.url + '</li>');
+	items.push('<li><strong>Environment:</strong> ' + result.environment + '</li>');
+	items.push('<li><strong>Requested results:</strong> ' + result.variables + '</li>');
+	items.push('<li><strong>Status:</strong> ' + result.status + '</li>');
+	items.push('<li><strong>Result URLs:</strong></br>')
+	items.push('<ol>')
+	$.each(result.results, function(key, val){
+            items.push('<li>' + val + '</li>');
+	});
+	items.push('</ol>')
+	items.push('</li>')
 
-    var items = [];
-    items.push('<li><strong>Name:</strong> ' + result.name + '</li>');
-    items.push('<li><strong>Owner:</strong> ' + result.owner + '</li>');
-    items.push('<li><strong>Resource URL:</strong> ' + result.url + '</li>');
-    items.push('<li><strong>Environment:</strong> ' + result.environment + '</li>');
-    items.push('<li><strong>Requested results:</strong> ' + result.variables + '</li>');
-    items.push('<li><strong>Status:</strong> ' + result.status + '</li>');
-    items.push('<li><strong>Result URLs:</strong></br>')
-    items.push('<ol>')
-    $.each(result.results, function(key, val){
-        items.push('<li>' + val + '</li>');
-    });
-    items.push('</ol>')
-    items.push('</li>')
+	$('<ul/>', {
+            'class': 'my-new-list',
+            html: items.join('')
+	}).appendTo('#jobinfo');
 
-    $('<ul/>', {
-        'class': 'my-new-list',
-        html: items.join('')
-    }).appendTo('#jobinfo');
+
+	var solution = new X.mesh();
+	solution.file = result.results[0];
+	solution.magicmode = true;
+	r.add(solution);
+	r.render();
+    }
+
 
     // create and initialize a 3D renderer
     var r = new X.renderer3D();
     r.container = 'visualization';
     r.init();
-
-    // create a new X.mesh
-    var solution = new X.mesh();
-    solution.file = result.results[0];
-    solution.caption = result.name;
-    solution.magicmode = true;
-
-    // .. add the mesh
-    r.add(solution);
-
-    // re-position the camera to face the solution
     r.camera.position = [0, 0, 2.5];
     r.camera.focus = [0, 0, 0];
 
-    r.render();
+    result = $.getValues("http://localhost:8000/jobs/1/");
+    renderValues(result);
 
 };
