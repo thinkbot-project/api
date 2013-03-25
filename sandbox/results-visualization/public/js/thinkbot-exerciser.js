87863aea3a4e1bc5c504475e44f525f8dfe00e9a
Harish Narayanan <hnarayanan@gmail.com>
2013-03-25 18:06:29 +0100

    Improve the functionality of the sandboxed visualizer

---
+++ sandbox/results-visualization/public/js/thinkbot-exerciser.js
@@ -0,0 +1,63 @@
+window.onload = function() {
+
+    $.getJSON("http://localhost:8000/jobs/1/", function(data) {
+	var items = [];
+	items.push('<li><strong>Name:</strong> ' + data.name + '</li>');
+	items.push('<li><strong>Owner:</strong> ' + data.owner + '</li>');
+	items.push('<li><strong>Resource URL:</strong> ' + data.url + '</li>');
+	items.push('<li><strong>Environment:</strong> ' + data.environment + '</li>');
+	items.push('<li><strong>Requested results:</strong> ' + data.variables + '</li>');
+	items.push('<li><strong>Status:</strong> ' + data.status + '</li>');
+	items.push('<li><strong>Results URLs:</strong></br>')
+	items.push('<ol>')
+	$.each(data.results, function(key, val){
+            items.push('<li>' + val + '</li>');
+	});
+	items.push('</ol>')
+	items.push('</li>')
+
+	$('<ul/>', {
+            'class': 'my-new-list',
+            html: items.join('')
+	}).appendTo('#jobinfo');
+    });
+
+
+    jQuery.extend({
+	getValues: function(url) {
+            var result = null;
+            $.ajax({
+		url: url,
+		type: 'get',
+		dataType: 'json',
+		async: false,
+		success: function(data) {
+                    result = data;
+		}
+            });
+	    return result;
+	}
+    });
+    result = $.getValues("http://localhost:8000/jobs/1/")
+
+    // create and initialize a 3D renderer
+    var r = new X.renderer3D();
+    r.container = 'visualization';
+    r.init();
+
+    // create a new X.mesh
+    var solution = new X.mesh();
+    solution.file = result.results[0];
+    solution.caption = result.name;
+    solution.magicmode = true;
+
+    // .. add the mesh
+    r.add(solution);
+
+    // re-position the camera to face the solution
+    r.camera.position = [0, 0, 2.5];
+    r.camera.focus = [0, 0, 0];
+
+    r.render();
+
+};
