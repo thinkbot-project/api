41745ac0e3a0bb2201edfc545ec303e4fe8f6ef3
Harish Narayanan <hnarayanan@gmail.com>
2013-04-08 15:07:17 +0200

    Start work on the actual documentation of the API

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,7 @@ html_theme_path = templates_path
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "thinkbot docs"
+html_title = "thinkbot beta docs"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
