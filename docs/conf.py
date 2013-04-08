32acaeaea6e1b144ec684f6cd763b836962e1db4
Harish Narayanan <hnarayanan@gmail.com>
2013-04-08 13:44:09 +0200

    Move from rtd sphinx theme to customized armstrong theme

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ exclude_patterns = ['build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'manni'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -73,7 +73,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'rtd'
+html_theme = 'thinkbot'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
