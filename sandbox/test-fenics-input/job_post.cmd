cfe8e39a0949bab6c29427ccdeb99df8ee84abb0
Harish Narayanan <hnarayanan@gmail.com>
2013-06-09 17:18:00 +0200

    Add a test fenics input file with token header auth

--- sandbox/test-fenics-input/job_post.cmd
+++ sandbox/test-fenics-input/job_post.cmd
@@ -3,3 +3,11 @@ curl -i -X POST http://127.0.0.1:8000/jobs/ -u "harish" \
      -F "environment=fenics11" \
      -F "code=<demo_hyperelasticity.py" \
      -F "variables=u.vtk"
+
+
+curl -i -X POST http://127.0.0.1:8001/jobs/ -H "Authorization:  Token cf91179259e3715c99791d7865af259766167da8" \
+     -F "name=Hyperelasticity from curl with token" \
+     -F "environment=fenics11" \
+     -F "code=<demo_hyperelasticity.py" \
+     -F "variables=u.vtk"
+
