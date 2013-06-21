c3a6cdedb07873dbf56eaeb29a98bc5861c9ff93
Harish Narayanan <hnarayanan@gmail.com>
2013-06-21 21:42:21 +0200

    Fix production update script

--- scripts/updateproduction.sh
+++ scripts/updateproduction.sh
@@ -1,9 +1,10 @@
-cd ~/sites/mechanicsacademy.com
+cd ~/sites/thinkbot.net
 git pull
 source venv/bin/activate
 source config/secrets/production.sh
 pip install -r requirements.txt
-cd lms
+cd compute_service
 ./manage.py migrate
 ./manage.py collectstatic --noinput
 # restart uwsgi
+# restart celery
